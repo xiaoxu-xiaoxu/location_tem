2890c152f61b415bb31e92c7df9c01e7d0d690dc
xiaoxu-xiaoxu <2411711641@qq.com>
2021-04-20 21:45:13 +0800

    end

--- src/main/java/com/xiaoxu/LocationTem.java
+++ src/main/java/com/xiaoxu/LocationTem.java
@@ -14,11 +14,11 @@ import java.util.Optional;
  */
 public class LocationTem{
 
-    private Map<String, String> province = new HashMap<>(64);
+    private final Map<String, String> province = new HashMap<>(64);
 
-    private Map<String, String> city = new HashMap<>(512);
+    private final Map<String, String> city = new HashMap<>(512);
 
-    private Map<String, String> area = new HashMap<>(1024);
+    private final Map<String, String> area = new HashMap<>(1024);
 
     private static final String CHINA_PROVINCE = "http://www.weather.com.cn/data/city3jdata/china.html";
 
@@ -30,7 +30,19 @@ public class LocationTem{
 
     private static final String SUFFIX = ".html";
 
-    public LocationTem(){
+    private static final Double NOT_EXISTS = -10000.0;
+
+    private static final Double API_TIME_OUT = -11111.0;
+
+    private static final LocationTem instance = new LocationTem();
+
+
+    public static LocationTem getInstance(){
+        return instance;
+    }
+
+
+    private LocationTem(){
         String str = HttpUtil.get(CHINA_PROVINCE);
         JSONObject obj = JSONUtil.parseObj(str);
         for(Map.Entry<String, Object> entry : obj.entrySet()){
@@ -39,29 +51,36 @@ public class LocationTem{
     }
 
 
+    /**
+     *
+     * @param province
+     * @param city
+     * @param county
+     * @return -10000.0 省 市 区 名字错误或不存在   -11111.0 调用查询温度api超时
+     */
     public Optional<Double> getTemperature(String province, String city, String county){
         String cityCode, areaCode,proCode;
         if((proCode = this.province.get(province)) == null){
-            return Optional.empty();
+            return Optional.of(NOT_EXISTS);
         }
         if((cityCode = this.city.get(province + city)) == null){
             // 如果没有，添加对应code
             initData(this.city, proCode, province, 1);
             if((cityCode = this.city.get(province + city)) == null){
-                return Optional.empty();
+                return Optional.of(NOT_EXISTS);
             }
         }
         if((areaCode = this.area.get(province + city + county)) == null){
             initData(this.area, proCode + cityCode, province + city, 2);
             if((areaCode = this.area.get(province + city + county)) == null){
-                return Optional.empty();
+                return Optional.of(NOT_EXISTS);
             }
         }
         String result = HttpUtil.get(LOCATION_TEM + proCode + cityCode + areaCode + SUFFIX);
         JSONObject obj = JSONUtil.parseObj(result);
         String weatherInfo = obj.getStr("weatherinfo");
         if(weatherInfo == null){
-            return Optional.empty();
+            return Optional.of(API_TIME_OUT);
         }
         JSONObject jsonObject = JSONUtil.parseObj(weatherInfo);
         String temp = jsonObject.getStr("temp");
